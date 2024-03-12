708fda2f24b8dc5c2a43c1165af3a53469e81035
heclair@hotmail.com <heclair@hotmail.com>
2024-03-11 22:16:46 -0300

    aula 11-03

--- test/CptecMock.test.ts
+++ test/CptecMock.test.ts
@@ -1,9 +1,20 @@
-import { Request, Response } from "express";
+import { NextFunction, Request, Response } from "express";
 import request from "supertest";
 import PrevisaoController from "../src/controllers/PrevisaoController";
 import Cptec from "../src/services/Cptec";
 import { parseString } from 'xml2js';
 
+
+jest.mock("../src/services/api", () => {});
+
+it("Lista cidades", async() =>{
+    const cptec = new Cptec();
+    const r = await cptec.listaCidades("santa branca");
+    expect(r).toMatch(/<nome>Santa Branca<\/nome>/i);
+});
+
+
+/*
 describe("Cptec", () => {
     test("Lista Cidade", async () => {
         const cptec = new Cptec();
@@ -25,24 +36,5 @@ describe("Cptec", () => {
        );
     });
 
-    test("Previsão 7 dias", async () => {
-        const cptec = new Cptec();
-        
-        const previsao = await cptec.previsao7dias("4528");
-        
-        expect(previsao).toEqual(
-        expect.stringContaining("<nome>Santa Branca</nome>")
-       );
-    });
-
-    test("Previsão Estendida", async () => {
-        const cptec = new Cptec();
-        
-        const previsao = await cptec.previsaoEstendida("4528");
-        
-        expect(previsao).toEqual(
-        expect.stringContaining("<nome>Santa Branca</nome>")
-       );
-    });
-
 });
+*/
